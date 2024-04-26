9776bb8f56e04762ae54b735c69d0a0d03bb43f9
Josh Massa <joshmassa@gmail.com>
2024-04-26 15:14:21 -0400

    Added model index, added User model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true, // Username must not already exist in the database
+            required: true, // Makes username a required field
+            trim: true // Remove whitespace from both sides of the string
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ // Regular expression to match an email address
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'friend'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
+
+const User = model('user', userSchema);
+
+module.exports = User;
